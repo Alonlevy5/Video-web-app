02377c7a2d3b01f9793cf62c4eb05bbe3ed68ae4
alonlevy5 <alonlevy5@gmail.com>
2021-11-14 17:51:59 +0200

    created view and installed packages

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
-hel
+const server = require('http').Server(app);
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('room');
+})
+
+
+
+
+server.listen(3030);
